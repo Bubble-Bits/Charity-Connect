e87cec7b6f0a39c659d3aa9c7f5921ed77a69048
Wisesofthemall <foxxydieujuste@gmail.com>
2023-06-16 09:56:02 -0400

    CD Test commit

--- charity-connect/example.test.ts
+++
@@ -1,7 +0,0 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
-test("add() should add two numbers correctly", () => {
-  expect(add(2, 3)).toBe(5);
-  expect(add(3, 7)).toBe(10);
-});
