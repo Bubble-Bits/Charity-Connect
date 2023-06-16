650a758d9ccc484c9dff209cb2a1da8465834a93
Wisesofthemall <foxxydieujuste@gmail.com>
2023-06-15 23:23:47 -0400

    Templates

---
+++ charity-connect/app/FAuth/Login.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+function Login({}: Props) {
+  return <div>Login</div>;
+}
+
+export default Login;
---
+++ charity-connect/app/homepage/homePage.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+function homePage({}: Props) {
+  return <div>homePage</div>;
+}
+
+export default homePage;
