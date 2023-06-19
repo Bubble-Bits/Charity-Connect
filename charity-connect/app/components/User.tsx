aefcc0c32e746a82c6e393de9f9f08e824b98431
Stanley Chu <chustanleys@gmail.com>
2023-06-18 19:30:33 -0700

    testing comment

--- charity-connect/app/components/User.tsx
+++ charity-connect/app/components/User.tsx
@@ -3,6 +3,7 @@ import { BiUserCircle } from "react-icons/bi";
 type Props = {};
 
 function User({}: Props) {
+  /* comment */
   return (
     <div className="flex flex-row items-center gap-1">
       <div className="p-2 bg-green-500 rounded-full text-black">
