1b3e70402759b7a20d115c701f9307bd5915509f
Anthony Bui <anthonyb50@gmail.com>
2023-06-20 23:57:14 -0400

    CSS Positioning and styling

--- charity-connect/pages/donate.tsx
+++ charity-connect/pages/donate.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from "react";
 import "../app/globals.css";
-import Calendar from "../app/components/Donation/Calendar.jsx"
+import Calendar from "../app/components/Donation/Calendar.jsx";
+import NavBar from "../app/components/Navbar.tsx"
 
 type Props = {};
 
@@ -14,29 +15,34 @@ function Donate({}: Props) {
 
   return (
     // <div className="fixed w-full bg-[#01002e] z-10 shadow-sm text-white justify-items-center">
-      <div className="flex flex-col justify-evenly align-middle w-full h-full border-[5px]">
-        <label htmlFor="description" >Description</label>
-        <input type="text" name="description" />
-        <label htmlFor="tenure" >Time Owned</label>
-        <input type="text" name="tenure" />
-        <label htmlFor="location" >Location</label>
-        <input type="text" name="location" />
-        <label htmlFor="features" >Features</label>
-        <input type="text" name="features" />
-        <label htmlFor="cargory" >Category</label>
-        <input type="text" name="category" />
-        <label htmlFor="delivery" >Delivery</label>
-        <input type="text" name="delivery" />
-        <Calendar
-        options={{
-          value: calendarValue,
-          onchange: handleCalendarChange,
-          readonly: false,
-          placeholder: "Choose a date"
-        }}
-        />
-        <button>Donate</button>
+    <>
+      <NavBar />
+      <div className="flex justify-center w-full h-full">
+        <div className="absolute top-24 flex flex-col justify-evenly w-1/2 h-3/5 border-[5px]">
+          <label htmlFor="description" >Description</label>
+          <input type="text" name="description" />
+          <label htmlFor="tenure" >Time Owned</label>
+          <input type="text" name="tenure" />
+          <label htmlFor="location" >Location</label>
+          <input type="text" name="location" />
+          <label htmlFor="features" >Features</label>
+          <input type="text" name="features" />
+          <label htmlFor="cargory" >Category</label>
+          <input type="text" name="category" />
+          <label htmlFor="delivery" >Delivery</label>
+          <input type="text" name="delivery" />
+          <Calendar
+          options={{
+            value: calendarValue,
+            onchange: handleCalendarChange,
+            readonly: false,
+            placeholder: "Choose a date"
+          }}
+          />
+          <button>Donate</button>
+        </div>
       </div>
+      </>
   );
 }
 
