3dc1f8cfabe6554d33b7e9d86267d499616a7288
Alvin <caskgar@gmail.com>
2023-06-21 02:50:18 -0400

    Added API call to that takes in address input

--- charity-connect/pages/map.tsx
+++ charity-connect/pages/map.tsx
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from "react";
+import exampleData from "./exampleData.js";
+import axios from "axios";
+require("dotenv").config();
 
 type Props = {
   long: number;
   lat: number;
+  user_address: string;
 };
 
-const LocationComponent = ({ long, lat }: Props) => {
+const input = "6823 Vista Place, Brooklyn NY 11220";
+const pw = process.env.GeoLocation;
+
+const LocationComponent = ({ long, lat, user_address }: Props) => {
   const [longitude, setLong] = useState(0);
   const [latitude, setLat] = useState(0);
   const [address, setAddress] = useState("");
 
   useEffect(() => {
-    setLong(40.6328942);
-    setLat(-74.019474);
-    setAddress("6823 Vista Place, Brooklyn NY 11220");
+    // axios
+    //   .get(`https://api.opencagedata.com/geocode/v1/json?q=${input}&key=${pw}`)
+    //   .then((response) => {
+    //     console.log(response);
+    //     setLong(response.data.results[0].geometry.lng);
+    //     setLat(response.data.results[0].geometry.lat);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    console.log("pwpwpwpwpw", pw);
+    // console.log(exampleData.data.results[0].geometry);
+    // setLong(exampleData.data.results[0].geometry.lng);
+    // setLat(exampleData.data.results[0].geometry.lat);
   }, []);
 
   return (
