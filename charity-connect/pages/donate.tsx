2a91b5ce240b423bbd9ac1ac308d6d2143f3b165
Anthony Bui <anthonyb50@gmail.com>
2023-06-22 11:49:10 -0400

    Empty api file, added state management, removed bug from typescript

--- charity-connect/pages/donate.tsx
+++ charity-connect/pages/donate.tsx
@@ -7,9 +7,18 @@ import NavBar from "../app/components/Navbar"
 type Props = {};
 
 function Donate({}: Props) {
+  const categories: string[] = ['Apparel', 'Electronics', 'Entertainment', 'Family', 'Garden and Outdoors', 'Hobbies', 'Homegoods', 'Music',
+  'Sports', 'Supplies', 'Toys and Games', 'Vehicles', 'Other']
+
   const [opened, openModal] = useState(false);
-  const [owned, setOwned] = useState(`${new Date()}`)
-  // const
+
+  const [images, setImages] = useState([]);
+  const [description, setDescription] = useState('');
+  const [owned, setOwned] = useState(`${new Date()}`);
+  const [address, setAddress] = useState('');
+  const [features, setFeatures] = useState('');
+  const [category, setCategory] = useState('Apparel');
+  const [delivery, setDelivery] = useState('Pickup');
 
   const textStyle: string="text-white";
   const textInput: string="w-4/5 rounded peer";
@@ -22,17 +31,21 @@ function Donate({}: Props) {
           <label htmlFor="images" className={textStyle}>Upload Images</label>
           <input type="file" className={textStyle} name="images"></input>
           <label htmlFor="description" className={textStyle}>Description</label>
-          <textarea name="description" rows="3" col="50" className={textInput}></textarea>
-          <button onClick={()=>{openModal(!opened)}} className="text-white bg-green-500 w-1/2 rounded">Select Date Owned</button>
+          <textarea name="description" rows={3} className={textInput} placeholder="Insert Description Here" onChange={(e)=>{setDescription(e.target.value)}}></textarea>
+          <button onClick={()=>{openModal(!opened)}} className="text-white bg-green-500 w-1/2 rounded hover:bg-green-700">Select Date Owned</button>
           <input type="text" name="tenure" disabled className={textInput} placeholder={owned}/>
           <label htmlFor="address" className={textStyle}>Address</label>
-          <input type="text" name="address" className={textInput}/>
+          <input type="text" name="address" className={textInput} onChange={(e)=>{setAddress(e.target.value)}}/>
           <label htmlFor="features" className={textStyle}>Features</label>
-          <input type="text" name="features" className={textInput}/>
-          <label htmlFor="cargory" className={textStyle}>Category</label>
-          <input type="text" name="category" className={textInput}/>
-          <label htmlFor="delivery" className={textStyle}>Preferred Delivery</label>
-          <select name="delivery" className={textInput}>
+          <input type="text" name="features" className={textInput} placeholder="List Features Here Separate By Commas" onChange={(e)=>{setFeatures(e.target.value)}}/>
+          <label htmlFor="category" className={textStyle}>Category</label>
+          <select name="category" className={textInput} onChange={(e)=>{setCategory(e.target.value)}}>
+            {categories.map(category => (
+              <option value={category} key={category}>{category}</option>
+            ))}
+          </select>
+          <label htmlFor="delivery" className={textStyle}>Preferred Delivery Method</label>
+          <select name="delivery" className={textInput} onChange={(e)=>{setDelivery(e.target.value)}}>
             <option value="pickup">Pickup</option>
             <option value="shipping">Shipping</option>
           </select>
@@ -41,7 +54,7 @@ function Donate({}: Props) {
             <Calendar onChange={(e: any)=> {setOwned(e); openModal(!opened);}}/>
           </div>
           : null}
-          <button className="text-white bg-green-500 w-4/5 rounded">Donate</button>
+          <button className="text-white bg-green-500 w-4/5 rounded hover:bg-green-700">Donate</button>
         </div>
       </div>
       </div>
