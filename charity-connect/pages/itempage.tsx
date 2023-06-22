028c384fce611f720b3925a4664c71f7466e4c11
Stanley Chu <chustanleys@gmail.com>
2023-06-22 16:18:07 -0700

    skeleton for mobilization complete

--- charity-connect/pages/itempage.tsx
+++ charity-connect/pages/itempage.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Navbar from "../app/components/Navbar";
 import "@/app/globals.css";
@@ -28,78 +30,105 @@ export default function itemPage() {
 
   */
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     // This keeps it fixed in the div. How do we keep everything inside of the div?
-    <div className="fixed inset-0 border border-red-500">
+    <div className="border border-red-500">
       <Navbar />
 
-      <div className="pt-24 md:grid grid-cols-7 h-full">
+      <div className="pt-24 md:grid grid-cols-7 border border-red-500">
         <div className="space-y-2 col-span-5 pb-4 ">
           <div className="justify-center flex border border-red-500 ">
-            Main Image
+            <img src="https://www.tutorialspoint.com/assets/questions/media/426142-1668760872.png"></img>
           </div>
           <div className="justify-center flex border border-red-500 ">
             Item gallery boxes
           </div>
         </div>
 
-        <div className="space-y-2 col-span-2 bg-[#01002e] z-10 shadow-sm p-5 h-screen max-h-screen">
-          <div className="h-5/6 overflow-y-scroll scrollbar-hide">
-            <div className="text-xl text-green-500">
-              {" "}
-              2017 Lamborghini Diablo
+        <div className="space-y-2 col-span-2 bg-[#01002e] z-10 shadow-sm p-5 border border-red-500">
+          {/* temporary: when md gets hit, remove the height and overflow y scroll and just let it overextend in the mobile aspect */}
+          <div className="scrollbar-hide">
+            <h1 className="text-green-500 text-4xl">teddy bear</h1>
+            <h1 className="text-white text-md">Available</h1>
+
+            <h1 className="text-white text-md pb-5">
+              listed 2 days ago in San Francisco
+            </h1>
+            <h1 className="text-green-500 text-xl">Item Description</h1>
+            <div
+              className={
+                isExpanded
+                  ? "text-white pb-1 max-h-full"
+                  : "text-white pb-1 max-h-16 overflow-hidden"
+              }
+            >
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
+              testing testing testing testing testing testing testing testing
             </div>
 
-            <div className="text-md  text-white">Available</div>
-
-            <div className="text-sm pb-5 text-white">
-              Listed 2 days ago in San Francisco
-            </div>
-
-            <div className="text-xl  text-green-500">Item Description</div>
-
-            <div className="text-md pb-5  text-white">
-              This class sets a maximum height for the element, and any content
-              that exceeds this height will be hidden due to the overflow-hidden
-              utility class. This class sets a maximum height for the element,
-              and any content that exceeds this height will be hidden due to the
-              overflow-hidden utility class. This class sets a maximum height
-              for the element, and any content that exceeds this height will be
-              hidden due to the overflow-hidden utility class. This class sets a
-              maximum height for the element, and any content that exceeds this
-              height will be hidden due to the overflow-hidden utility class.
-              This class sets a maximum height for the element, and any content
-              that exceeds this height will be hidden due to the overflow-hidden
-              utility class. This class sets a maximum height for the element,
-              and any content that exceeds this height will be hidden due to the
-              overflow-hidden utility class. This class sets a maximum height
-              for the element, and any content that exceeds this height will be
-              hidden due to the overflow-hidden utility class. This class sets a
-              maximum height for the element, and any content that exceeds this
-              height will be hidden due to the overflow-hidden utility class.
-              This class sets a maximum height for the element, and any content
-              that exceeds this height will be hidden due to the overflow-hidden
-              utility class. This class sets a maximum height for the element,
-              and any content that exceeds this height will be hidden due to the
-              overflow-hidden utility class. This class sets a maximum height
-              for the element, and any content that exceeds this height will be
-              hidden due to the overflow-hidden utility class. This class sets a
-              maximum height for the element, and any content that exceeds this
-              height will be hidden due to the overflow-hidden utility class.
-            </div>
+            {!isExpanded && (
+              <button
+                className="text-blue-500 pt-1 pb-5"
+                onClick={toggleDescription}
+              >
+                See More
+              </button>
+            )}
+            {isExpanded && (
+              <button
+                className="text-blue-500 pt-1 pb-5"
+                onClick={toggleDescription}
+              >
+                See Less
+              </button>
+            )}
 
             <div className="text-xl flex items-center pb-5">
               <div className="w-20 h-20 bg-gray-500 rounded-full inline-block"></div>
               <div className="inline-block pl-5  text-white">User Profile</div>
             </div>
 
-            <div className="text-xl text-green-500">Location</div>
+            <h1 className="text-green-500 text-xl">Location</h1>
           </div>
 
-          <div className="  w-full fixed bottom-0 bg-[#01002e] z-10 shadow-sm text-xl border-[red]">
-            <div className="text-white pb-5">Message</div>
+          <div className="bg-[#01002e] z-10 shadow-sm text-xl border-[red]">
+            <div className="text-white pb-1">Message</div>
             <textarea
-              className="w-10/12 rounded-lg  h-min overflow-y-scroll text-black placeholder-gray-400"
+              className="w-full rounded-lg h-min overflow-y-scroll text-black placeholder-gray-400"
               placeholder="Send a message to the donor!"
             ></textarea>
             <button className=" text-md rounded-lg bg-gray-500 w-full flex justify-center  text-white">
