5a8e1080e27b210ca0be5046e6b52c9867f7b3d0
Anthony Bui <anthonyb50@gmail.com>
2023-06-22 12:25:52 -0400

    Added axios dependency, structured donation page for readability, templating items api

---
+++ charity-connect/pages/api/items.ts
@@ -0,0 +1,9 @@
+export default function handler(req: any, res: any) {
+  if (req.method === "GET") {
+
+  } else if (req.method === "POST") {
+
+  } else if (req.method === "PUT") {
+
+  }
+}
--- charity-connect/pages/donate.tsx
+++ charity-connect/pages/donate.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import "../app/globals.css";
 import Calendar from 'react-calendar';
-import NavBar from "../app/components/Navbar"
+import NavBar from "../app/components/Navbar";
 
 type Props = {};
 
@@ -13,6 +13,7 @@ function Donate({}: Props) {
   const [opened, openModal] = useState(false);
 
   const [images, setImages] = useState([]);
+  const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [owned, setOwned] = useState(`${new Date()}`);
   const [address, setAddress] = useState('');
@@ -28,36 +29,48 @@ function Donate({}: Props) {
       <NavBar />
       <div className="flex justify-center w-full h-full">
         <div className="absolute top-20 flex flex-col justify-evenly items-center w-screen h-screen border-[5px] bg-[#01002e]">
+
           <label htmlFor="images" className={textStyle}>Upload Images</label>
           <input type="file" className={textStyle} name="images"></input>
+
+          <label htmlFor="name" className={textStyle}>Item Name</label>
+          <input type="text" name="name" className={textInput} onChange={(e)=>{setName(e.target.value)}}/>
+
           <label htmlFor="description" className={textStyle}>Description</label>
           <textarea name="description" rows={3} className={textInput} placeholder="Insert Description Here" onChange={(e)=>{setDescription(e.target.value)}}></textarea>
+
           <button onClick={()=>{openModal(!opened)}} className="text-white bg-green-500 w-1/2 rounded hover:bg-green-700">Select Date Owned</button>
           <input type="text" name="tenure" disabled className={textInput} placeholder={owned}/>
+          {opened ?
+          <div className="absolute flex justify-center items-center w-full h-full bg-black/50">
+            <Calendar className="bg-white w-1/4 h-1/4" onChange={(e: any)=> {setOwned(e); openModal(!opened);}}/>
+          </div>
+          : null}
+
           <label htmlFor="address" className={textStyle}>Address</label>
           <input type="text" name="address" className={textInput} onChange={(e)=>{setAddress(e.target.value)}}/>
+
           <label htmlFor="features" className={textStyle}>Features</label>
           <input type="text" name="features" className={textInput} placeholder="List Features Here Separate By Commas" onChange={(e)=>{setFeatures(e.target.value)}}/>
+
           <label htmlFor="category" className={textStyle}>Category</label>
           <select name="category" className={textInput} onChange={(e)=>{setCategory(e.target.value)}}>
             {categories.map(category => (
               <option value={category} key={category}>{category}</option>
             ))}
           </select>
+
           <label htmlFor="delivery" className={textStyle}>Preferred Delivery Method</label>
           <select name="delivery" className={textInput} onChange={(e)=>{setDelivery(e.target.value)}}>
             <option value="pickup">Pickup</option>
             <option value="shipping">Shipping</option>
           </select>
-          {opened ?
-          <div className="absolute flex justify-center items-center w-full h-full bg-white">
-            <Calendar onChange={(e: any)=> {setOwned(e); openModal(!opened);}}/>
-          </div>
-          : null}
+
           <button className="text-white bg-green-500 w-4/5 rounded hover:bg-green-700">Donate</button>
+
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
