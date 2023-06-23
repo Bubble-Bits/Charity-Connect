910d2ad75e76d8751c719bb1526381f0e228494b
Stanley Chu <chustanleys@gmail.com>
2023-06-22 23:53:30 -0700

    adjusted useState for vercel error

--- charity-connect/pages/itempage.tsx
+++ charity-connect/pages/itempage.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Navbar from "../app/components/Navbar";
 import "@/app/globals.css";
 
-export default function itemPage() {
+export default function ItemPage() {
   /*
 
   Grid = create the grid
@@ -38,20 +38,18 @@ export default function itemPage() {
 
   return (
     // This keeps it fixed in the div. How do we keep everything inside of the div?
-    <div className="border border-red-500">
+    <div className="">
       <Navbar />
 
-      <div className="pt-24 md:grid grid-cols-7 border border-red-500">
+      <div className="pt-24 md:grid grid-cols-7">
         <div className="space-y-2 col-span-5 pb-4 ">
-          <div className="justify-center flex border border-red-500 ">
+          <div className="justify-center flex ">
             <img src="https://www.tutorialspoint.com/assets/questions/media/426142-1668760872.png"></img>
           </div>
-          <div className="justify-center flex border border-red-500 ">
-            Item gallery boxes
-          </div>
+          <div className="justify-center flex ">Item gallery boxes</div>
         </div>
 
-        <div className="space-y-2 col-span-2 bg-[#01002e] z-10 shadow-sm p-5 border border-red-500">
+        <div className="space-y-2 col-span-2 bg-[#01002e] z-10 shadow-sm p-5">
           {/* temporary: when md gets hit, remove the height and overflow y scroll and just let it overextend in the mobile aspect */}
           <div className="scrollbar-hide">
             <h1 className="text-green-500 text-4xl">teddy bear</h1>
@@ -125,7 +123,7 @@ export default function itemPage() {
             <h1 className="text-green-500 text-xl">Location</h1>
           </div>
 
-          <div className="bg-[#01002e] z-10 shadow-sm text-xl border-[red]">
+          <div className="bg-[#01002e] z-10 shadow-sm text-xl">
             <div className="text-white pb-1">Message</div>
             <textarea
               className="w-full rounded-lg h-min overflow-y-scroll text-black placeholder-gray-400"
