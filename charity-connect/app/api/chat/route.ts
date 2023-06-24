4a5215b23b213a48e8866556bfe54320a4e4edc2
Nickmont3 <nickmonteleone25@gmail.com>
2023-06-23 20:39:59 -0700

    Update route.ts

--- charity-connect/app/api/chat/route.ts
+++ charity-connect/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest | Request) {
   try {
     // const chatData: any = {
     //   userIds: { set: userIds },
-    //   messages: { 
+    //   messages: {  
     //     create: [],
     //   },
     // };
