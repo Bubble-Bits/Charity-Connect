4f3879f4f860b0b4be0ea5da19f2c7c62f02df43
Wisesofthemall <foxxydieujuste@gmail.com>
2023-06-24 14:18:14 -0400

    Image refactor

--- charity-connect/app/components/userprofile/UserProfile.tsx
+++ charity-connect/app/components/userprofile/UserProfile.tsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 import ProfileMenu from "./ProfileMenu";
 import Link from "next/link";
 import Logo from "../Logo";
-
-
+import Image from "next/image";
 
 export default function UserProfile() {
-
-  var imgLink = "https://www.interstatedevelopment.com/wp-content/uploads/2019/04/generic-avatar-1.jpg";
-  var claimedDonation = "https://img.freepik.com/premium-vector/donation-box-quarantine-time_291916-2.jpg?w=2000";
-  var postedDonation = "https://static.vecteezy.com/system/resources/thumbnails/001/234/040/small/cartoon-donation-box.jpg"
+  var imgLink =
+    "https://www.interstatedevelopment.com/wp-content/uploads/2019/04/generic-avatar-1.jpg";
+  var claimedDonation =
+    "https://img.freepik.com/premium-vector/donation-box-quarantine-time_291916-2.jpg?w=2000";
+  var postedDonation =
+    "https://static.vecteezy.com/system/resources/thumbnails/001/234/040/small/cartoon-donation-box.jpg";
 
   var user = {
     name: "Jane Doe",
@@ -26,7 +27,7 @@ export default function UserProfile() {
 
   const toggleImageShown = () => {
     setShowPostedDonations(!showPostedDonations);
-  }
+  };
 
   const [showPostedDonations, setShowPostedDonations] = useState(true);
   //bg-[#01002e]
@@ -40,57 +41,89 @@ export default function UserProfile() {
       </div>
       <div className="text-center h-full bg-[#01002e] overflow-y-auto text-white">
         <div className="flex items-center justify-center mt-4">
-          <img src={imgLink} alt="image" className="self-center w-40 h-40 rounded-full"></img>
+          <Image
+            src={imgLink}
+            alt="image"
+            className="self-center w-40 h-40 rounded-full"
+          />
         </div>
         <div className="m-4">
-          <button className="text-white bg-green-500 w-1/3 rounded hover:bg-green-700">update picture</button>
+          <button className="text-white bg-green-500 w-1/3 rounded hover:bg-green-700">
+            update picture
+          </button>
         </div>
-
 
         <label>Name</label>
         <br></br>
-        <input className="text-black bg-slate-200 p-1" defaultValue={user.name}></input>
+        <input
+          className="text-black bg-slate-200 p-1"
+          defaultValue={user.name}
+        ></input>
         <div className="m-4">
-          <button className="text-white bg-green-500 w-1/10 rounded hover:bg-green-700">update username</button>
+          <button className="text-white bg-green-500 w-1/10 rounded hover:bg-green-700">
+            update username
+          </button>
         </div>
-
-
 
         <label>Bio</label>
         <br></br>
-        <textarea className="text-black bg-slate-200 p-1" defaultValue={user.bio} rows={3} />
+        <textarea
+          className="text-black bg-slate-200 p-1"
+          defaultValue={user.bio}
+          rows={3}
+        />
         <div className="m-4">
-          <button className="text-white bg-green-500 w-1/10 rounded hover:bg-green-700">update bio</button>
+          <button className="text-white bg-green-500 w-1/10 rounded hover:bg-green-700">
+            update bio
+          </button>
         </div>
-
 
         <ProfileMenu onShowDonationClick={toggleImageShown} />
         <div className="grid grid-cols-2 grid-rows-3 gap-4 place-content-center h-200 p-4">
           <div className="bg-gray-500">
-            {showPostedDonations ?
-              <Link href="/donate">
-                Create New Post
-              </Link> :
-              <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Link href="/donate">Create New Post</Link>
+            ) : (
+              <Image src={claimedDonation} alt="claimDonation" />
+            )}
           </div>
           <div className="">
-            {showPostedDonations ? <img src={postedDonation}></img> : <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Image src={postedDonation} alt="post Donation" />
+            ) : (
+              <Image src={claimedDonation} alt="claimed Donation" />
+            )}
           </div>
           <div className="">
-            {showPostedDonations ? <img src={postedDonation}></img> : <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Image src={postedDonation} alt="Post Dontation" />
+            ) : (
+              <Image src={claimedDonation} alt="Claimed Donation" />
+            )}
           </div>
           <div className="">
-            {showPostedDonations ? <img src={postedDonation}></img> : <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Image src={postedDonation} alt="Posted Donation" />
+            ) : (
+              <Image src={claimedDonation} alt="Claimed Donation" />
+            )}
           </div>
           <div className="">
-            {showPostedDonations ? <img src={postedDonation}></img> : <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Image src={postedDonation} alt="Posted Donation" />
+            ) : (
+              <Image src={claimedDonation} alt="Claimed Donation" />
+            )}
           </div>
           <div className="">
-            {showPostedDonations ? <img src={postedDonation}></img> : <img src={claimedDonation}></img>}
+            {showPostedDonations ? (
+              <Image src={postedDonation} alt="Posted Donation" />
+            ) : (
+              <Image src={claimedDonation} alt="Claimed Donation" />
+            )}
           </div>
         </div>
       </div>
     </div>
   );
-
 }
