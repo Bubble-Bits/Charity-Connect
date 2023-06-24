66a0b259282fdb78f1b00acf56cb76f4cf33718b
Nickmont3 <nickmonteleone25@gmail.com>
2023-06-23 20:19:17 -0700

    Update route.ts

--- charity-connect/app/api/chat/route.ts
+++ charity-connect/app/api/chat/route.ts
@@ -1,27 +1,29 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
-// import { NextResponse, NextRequest } from "next/server";
-// import { Chat } from '.prisma/client';
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import { NextResponse, NextRequest } from "next/server";
+import { Chat } from '.prisma/client';
 
-// export async function POST(request: NextRequest | Request) {
-//   const bodyText = await request.text();
-//   const body = JSON.parse(bodyText);
-//   const { userIds } = body;
-//   try {
-//     const chatData: any = {
-//       userIds: { set: userIds },
-//       messages: [],
-//     };
-//     const chat: Chat = await prisma.chat.create({
-//       data: chatData,
-//       include: {
-//         messages: true,
-//       },
-//     });
+export async function POST(request: NextRequest | Request) {
+  const bodyText = await request.text();
+  const body = JSON.parse(bodyText);
+  const { userIds } = body;
+  try {
+    // const chatData: any = {
+    //   userIds: { set: userIds },
+    //   messages: [],
+    // };
+    // const chat: Chat = await prisma.chat.create({
+    //   data: chatData,
+    //   include: {
+    //     messages: true,
+    //   },
+    // });
 
-//     return NextResponse.json(chat);
-//   } catch (error) {
-//     console.error("Error creating chat:", error);
-//     return NextResponse.json(error);
-//   }
-// }
+    const chat = {temp: 'data'};
+
+    return NextResponse.json(chat);
+  } catch (error) {
+    console.error("Error creating chat:", error);
+    return NextResponse.json(error);
+  }
+}
