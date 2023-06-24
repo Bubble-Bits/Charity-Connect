67aa9845022d2e8b012254417f6ec8dcb4ace05b
Nickmont3 <nickmonteleone25@gmail.com>
2023-06-23 20:34:22 -0700

    commented out for now

--- charity-connect/app/api/addmap/route.ts
+++ charity-connect/app/api/addmap/route.ts
@@ -5,27 +5,27 @@ import User from "@/app/components/User";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const profile: User = {
-    id: "232323",
-    profilePic: "eddw",
-    name: "exaplmepo",
-    email: "mdw0i",
-    bio: "string",
-    address: ",d-w",
-    itemsClaimed: 0,
-    itemsSuccessClaimed: 0,
-    blocked: ["323"],
-  };
-  const user = await prisma.item.create({
-    data: {
-      name: "Alvin's ITEM!!!",
-      description: "From lalaland",
-      features: ["hello", "there"],
-      photos: ["HELLO!!!!"],
-      address: "6823 Vista Place Brooklyn NY 11220",
-      poster: { connect: { posterId: id } },
-      posterId: "6495fab420529e88f6120674",
-    },
-  });
-  return NextResponse.json(user);
+  // const profile: User = {
+  //   id: "232323",
+  //   profilePic: "eddw",
+  //   name: "exaplmepo",
+  //   email: "mdw0i",
+  //   bio: "string",
+  //   address: ",d-w",
+  //   itemsClaimed: 0,
+  //   itemsSuccessClaimed: 0,
+  //   blocked: ["323"],
+  // };
+  // const user = await prisma.item.create({
+  //   data: {
+  //     name: "Alvin's ITEM!!!",
+  //     description: "From lalaland",
+  //     features: ["hello", "there"],
+  //     photos: ["HELLO!!!!"],
+  //     address: "6823 Vista Place Brooklyn NY 11220",
+  //     poster: { connect: { posterId: id } },
+  //     posterId: "6495fab420529e88f6120674",
+  //   },
+  // });
+  return NextResponse.json({temp: 'data});
 }
