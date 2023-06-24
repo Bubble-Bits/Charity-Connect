ce466e1a4f1b4b7991e8b4233d82c86747e21379
Wisesofthemall <foxxydieujuste@gmail.com>
2023-06-23 20:43:00 -0400

    Comment out api

--- charity-connect/app/api/register/route.ts
+++ charity-connect/app/api/register/route.ts
@@ -1,10 +1,10 @@
-import { Request } from "node-fetch";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-import User from "@/app/components/User";
-import { NextResponse, NextRequest } from "next/server";
+// import { Request } from "node-fetch";
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+// import User from "@/app/components/User";
+// import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const user = {};
-  return NextResponse.json(user);
-}
+// export async function POST(request: NextRequest) {
+//   const user = {};
+//   return NextResponse.json(user);
+// }
