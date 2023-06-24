eab5182d6d4c9f382dce15f5127a9b87eab99dc9
Alvin <caskgar@gmail.com>
2023-06-23 22:38:49 -0400

    Merged

---
+++ charity-connect/pages/exampleData-address.js
@@ -0,0 +1,388 @@
+const exampleData = {
+  data: {
+    documentation: "https://opencagedata.com/api",
+    licenses: [
+      {
+        name: "see attribution guide",
+        url: "https://opencagedata.com/credits",
+      },
+    ],
+    rate: {
+      limit: 2500,
+      remaining: 2493,
+      reset: 1687392000,
+    },
+    results: [
+      {
+        annotations: {
+          DMS: {
+            lat: "40° 37' 58.41912'' N",
+            lng: "74° 1' 10.10640'' W",
+          },
+          FIPS: {
+            county: "36047",
+            state: "36",
+          },
+          MGRS: "18TWK8292098468",
+          Maidenhead: "FN20xp71pv",
+          Mercator: {
+            x: -8239810.151,
+            y: 4930512.009,
+          },
+          OSM: {
+            edit_url:
+              "https://www.openstreetmap.org/edit?way=248548003#map=16/40.63289/-74.01947",
+            note_url:
+              "https://www.openstreetmap.org/note/new#map=16/40.63289/-74.01947&layers=N",
+            url: "https://www.openstreetmap.org/?mlat=40.63289&mlon=-74.01947#map=16/40.63289/-74.01947",
+          },
+          UN_M49: {
+            regions: {
+              AMERICAS: "019",
+              NORTHERN_AMERICA: "021",
+              US: "840",
+              WORLD: "001",
+            },
+            statistical_groupings: ["MEDC"],
+          },
+          callingcode: 1,
+          currency: {
+            alternate_symbols: ["US$"],
+            decimal_mark: ".",
+            disambiguate_symbol: "US$",
+            html_entity: "$",
+            iso_code: "USD",
+            iso_numeric: "840",
+            name: "United States Dollar",
+            smallest_denomination: 1,
+            subunit: "Cent",
+            subunit_to_unit: 100,
+            symbol: "$",
+            symbol_first: 1,
+            thousands_separator: ",",
+          },
+          flag: "🇺🇸",
+          geohash: "dr5r5suxv0rz7z27kz9d",
+          qibla: 58.48,
+          roadinfo: {
+            drive_on: "right",
+            road: "Vista Place",
+            speed_in: "mph",
+          },
+          sun: {
+            rise: {
+              apparent: 1687339620,
+              astronomical: 1687332060,
+              civil: 1687337580,
+              nautical: 1687335060,
+            },
+            set: {
+              apparent: 1687307340,
+              astronomical: 1687314900,
+              civil: 1687309380,
+              nautical: 1687311900,
+            },
+          },
+          timezone: {
+            name: "America/New_York",
+            now_in_dst: 1,
+            offset_sec: -14400,
+            offset_string: "-0400",
+            short_name: "EDT",
+          },
+          what3words: {
+            words: "vote.values.organ",
+          },
+        },
+        bounds: {
+          northeast: {
+            lat: 40.6329702,
+            lng: -74.0193818,
+          },
+          southwest: {
+            lat: 40.6328373,
+            lng: -74.0196068,
+          },
+        },
+        components: {
+          "ISO_3166-1_alpha-2": "US",
+          "ISO_3166-1_alpha-3": "USA",
+          "ISO_3166-2": ["US-NY"],
+          _category: "building",
+          _type: "building",
+          borough: "Brooklyn",
+          city: "New York",
+          continent: "North America",
+          country: "United States",
+          country_code: "us",
+          county: "Kings County",
+          house_number: "6823",
+          neighbourhood: "Bay Ridge",
+          postcode: "11220",
+          road: "Vista Place",
+          state: "New York",
+          state_code: "NY",
+        },
+        confidence: 10,
+        formatted:
+          "6823 Vista Place, New York, NY 11220, United States of America",
+        geometry: {
+          lat: 40.6328942,
+          lng: -74.019474,
+        },
+      },
+      {
+        annotations: {
+          DMS: {
+            lat: "40° 37' 59.94840'' N",
+            lng: "74° 1' 9.40440'' W",
+          },
+          FIPS: {
+            state: "36",
+          },
+          MGRS: "18TWK8293698515",
+          Maidenhead: "FN20xp71qx",
+          Mercator: {
+            x: -8239788.447,
+            y: 4930574.073,
+          },
+          OSM: {
+            note_url:
+              "https://www.openstreetmap.org/note/new#map=16/40.63332/-74.01928&layers=N",
+            url: "https://www.openstreetmap.org/?mlat=40.63332&mlon=-74.01928#map=16/40.63332/-74.01928",
+          },
+          UN_M49: {
+            regions: {
+              AMERICAS: "019",
+              NORTHERN_AMERICA: "021",
+              US: "840",
+              WORLD: "001",
+            },
+            statistical_groupings: ["MEDC"],
+          },
+          callingcode: 1,
+          currency: {
+            alternate_symbols: ["US$"],
+            decimal_mark: ".",
+            disambiguate_symbol: "US$",
+            html_entity: "$",
+            iso_code: "USD",
+            iso_numeric: "840",
+            name: "United States Dollar",
+            smallest_denomination: 1,
+            subunit: "Cent",
+            subunit_to_unit: 100,
+            symbol: "$",
+            symbol_first: 1,
+            thousands_separator: ",",
+          },
+          flag: "🇺🇸",
+          geohash: "dr5r5thf1z9jpyu9wb22",
+          qibla: 58.48,
+          roadinfo: {
+            drive_on: "right",
+            road: "Vista Pl",
+            speed_in: "mph",
+          },
+          sun: {
+            rise: {
+              apparent: 1687339620,
+              astronomical: 1687332060,
+              civil: 1687337580,
+              nautical: 1687335060,
+            },
+            set: {
+              apparent: 1687307340,
+              astronomical: 1687314900,
+              civil: 1687309380,
+              nautical: 1687311900,
+            },
+          },
+          timezone: {
+            name: "America/New_York",
+            now_in_dst: 1,
+            offset_sec: -14400,
+            offset_string: "-0400",
+            short_name: "EDT",
+          },
+          what3words: {
+            words: "oils.tune.plots",
+          },
+        },
+        components: {
+          "ISO_3166-1_alpha-2": "US",
+          "ISO_3166-1_alpha-3": "USA",
+          "ISO_3166-2": ["US-NY"],
+          _category: "road",
+          _type: "road",
+          continent: "North America",
+          country: "United States of America",
+          country_code: "us",
+          road: "Vista Pl",
+          state: "New York",
+          state_code: "NY",
+          town: "Brooklyn",
+        },
+        confidence: 9,
+        formatted: "Vista Pl, Brooklyn, NY, United States of America",
+        geometry: {
+          lat: 40.633319,
+          lng: -74.019279,
+        },
+      },
+      {
+        annotations: {
+          DMS: {
+            lat: "40° 38' 28.32000'' N",
+            lng: "74° 0' 47.88000'' W",
+          },
+          FIPS: {
+            state: "36",
+          },
+          MGRS: "18TWK8343199396",
+          Maidenhead: "FN20xp83jv",
+          Mercator: {
+            x: -8239122.868,
+            y: 4931725.71,
+          },
+          OSM: {
+            note_url:
+              "https://www.openstreetmap.org/note/new#map=16/40.64120/-74.01330&layers=N",
+            url: "https://www.openstreetmap.org/?mlat=40.64120&mlon=-74.01330#map=16/40.64120/-74.01330",
+          },
+          UN_M49: {
+            regions: {
+              AMERICAS: "019",
+              NORTHERN_AMERICA: "021",
+              US: "840",
+              WORLD: "001",
+            },
+            statistical_groupings: ["MEDC"],
+          },
+          callingcode: 1,
+          currency: {
+            alternate_symbols: ["US$"],
+            decimal_mark: ".",
+            disambiguate_symbol: "US$",
+            html_entity: "$",
+            iso_code: "USD",
+            iso_numeric: "840",
+            name: "United States Dollar",
+            smallest_denomination: 1,
+            subunit: "Cent",
+            subunit_to_unit: 100,
+            symbol: "$",
+            symbol_first: 1,
+            thousands_separator: ",",
+          },
+          flag: "🇺🇸",
+          geohash: "dr5r5y90jhg90qsteew3",
+          qibla: 58.48,
+          roadinfo: {
+            drive_on: "right",
+            speed_in: "mph",
+          },
+          sun: {
+            rise: {
+              apparent: 1687339620,
+              astronomical: 1687332060,
+              civil: 1687337580,
+              nautical: 1687335060,
+            },
+            set: {
+              apparent: 1687307340,
+              astronomical: 1687314900,
+              civil: 1687309380,
+              nautical: 1687311900,
+            },
+          },
+          timezone: {
+            name: "America/New_York",
+            now_in_dst: 1,
+            offset_sec: -14400,
+            offset_string: "-0400",
+            short_name: "EDT",
+          },
+          what3words: {
+            words: "stores.wants.poppy",
+          },
+        },
+        bounds: {
+          northeast: {
+            lat: 40.652743,
+            lng: -73.997734,
+          },
+          southwest: {
+            lat: 40.628656,
+            lng: -74.038653,
+          },
+        },
+        components: {
+          "ISO_3166-1_alpha-2": "US",
+          "ISO_3166-1_alpha-3": "USA",
+          "ISO_3166-2": ["US-NY"],
+          _category: "postcode",
+          _type: "postcode",
+          continent: "North America",
+          country: "United States of America",
+          country_code: "us",
+          county: "Brooklyn",
+          postcode: "11220",
+          state: "New York",
+          state_code: "NY",
+        },
+        confidence: 7,
+        formatted: "Brooklyn, NY 11220, United States of America",
+        geometry: {
+          lat: 40.6412,
+          lng: -74.0133,
+        },
+      },
+    ],
+    status: {
+      code: 200,
+      message: "OK",
+    },
+    stay_informed: {
+      blog: "https://blog.opencagedata.com",
+      mastodon: "https://en.osm.town/@opencage",
+      twitter: "https://twitter.com/OpenCage",
+    },
+    thanks: "For using an OpenCage API",
+    timestamp: {
+      created_http: "Wed, 21 Jun 2023 06:37:04 GMT",
+      created_unix: 1687329424,
+    },
+    total_results: 3,
+  },
+  status: 200,
+  statusText: "",
+  headers: {
+    "content-length": "1728",
+    "content-type": "application/json; charset=utf-8",
+  },
+  config: {
+    transitional: {
+      silentJSONParsing: true,
+      forcedJSONParsing: true,
+      clarifyTimeoutError: false,
+    },
+    adapter: ["xhr", "http"],
+    transformRequest: [null],
+    transformResponse: [null],
+    timeout: 0,
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
+    maxContentLength: -1,
+    maxBodyLength: -1,
+    env: {},
+    headers: {
+      Accept: "application/json, text/plain, */*",
+    },
+    method: "get",
+    url: "https://api.opencagedata.com/geocode/v1/json?q=6823 Vista Place, Brooklyn NY 11220&key=6fd14a5df41c438a9da5255eae559f3e",
+  },
+  request: {},
+};
+
+export default exampleData;
