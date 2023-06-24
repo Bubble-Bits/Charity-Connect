c0d80b47550a1ff9cdf97186cb71409147ad5c7f
Christina Hunter <frijole16@gmail.com>
2023-06-24 01:09:34 -0700

    Restored to old ver for this file

--- charity-connect/app/page.tsx
+++ charity-connect/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       Charity-Connect
-      <Link href="/donate">Donate</Link>
+      <Link href="/donate">Click</Link>
     </main>
   );
 }
