76fdfdc60b65efcf61134835d11c44a4b7d929de
Anthony Bui <anthonyb50@gmail.com>
2023-06-27 19:30:37 -0400

    Templating for creating item

--- charity-connect/app/api/items/route.ts
+++ charity-connect/app/api/items/route.ts
@@ -1,7 +1,28 @@
 import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   // const body = await request.json();
+  const newItem = await prisma.item.create({
+    data: {
+      name: 'Alice',
+      category: 'alice@prisma.io',
+      description: ,
+      features: ,
+      timeOwned: ,
+      photos: ,
+      address: ,
+      pickup: ,
+      shipping: ,
+      status: ,
+      poster: ,
+      posterId: ,
+      claimer: ,
+      claimerId: ,
+    },
+  })
 
   return NextResponse.json({ message: "eko" });
 }
