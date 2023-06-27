60687524ced0c841ddbfcbbf08895e46f2b3081f
Anthony Bui <anthonyb50@gmail.com>
2023-06-27 19:22:37 -0400

    Added additional HTTP request methods

--- charity-connect/app/api/items/route.ts
+++ charity-connect/app/api/items/route.ts
@@ -1,16 +1,24 @@
-// export function POST(req: any, res: any) {
-//   if (req.method === 'POST') {
-//     // Process a POST request
-//     console.log('here');
-//     res.send(200);
-//   } else {
-//     // Handle any other HTTP method
-//   }
-// }
-
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(request: Request) {
+  // const body = await request.json();
+
+  return NextResponse.json({ message: "eko" });
+}
+
+export async function PUT(request: Request) {
+  // const body = await request.json();
+
+  return NextResponse.json({ message: "eko" });
+}
+
+export async function GET(request: Request) {
+  // const body = await request.json();
+
+  return NextResponse.json({ message: "eko" });
+}
+
+export async function DELETE(request: Request) {
   // const body = await request.json();
 
   return NextResponse.json({ message: "eko" });
