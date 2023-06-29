beeeff6f554a48ee8921d6ca4be3b8e3b31d07d5
Nickmont3 <nickmonteleone25@gmail.com>
2023-06-29 12:30:12 -0700

    commented out chat route for now because typesafety is so confusing

--- charity-connect/app/api/chat/route.ts
+++ charity-connect/app/api/chat/route.ts
@@ -3,30 +3,31 @@ const prisma = new PrismaClient();
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = request.nextUrl.searchParams;
-  const bodyText = body.get('userIds') as string;
-  const userIds = bodyText.split(',');
-  // const chatData: any = {
-  //   userIds: { set: userIds },0
-  //   messages: {
-  //     create: [],
-  //   },
-  // };
+  return NextResponse.json({'mess': 'hi'});
+  // const body = request.nextUrl.searchParams;
+  // const bodyText = body.get('userIds') as string;
+  // const userIds = bodyText.split(',');
+  // // const chatData: any = {
+  // //   userIds: { set: userIds },0
+  // //   messages: {
+  // //     create: [],
+  // //   },
+  // // };
 
-  try {
+  // try {
 
-    const chat = await prisma.chat.create({
-      data: {
-        userIds: {set: userIds}, messages: {create: []}
-      },
-      include: {
-        messages: true,
-      },
-    });
+  //   const chat = await prisma.chat.create({
+  //     data: {
+  //       userIds: {set: userIds}, messages: {create: []}
+  //     },
+  //     include: {
+  //       messages: true,
+  //     },
+  //   });
 
-    return NextResponse.json({chat});
-  } catch (error) {
-    console.error("Error creating chat:", error);
-    return NextResponse.json(error);
-  }
+  //   return NextResponse.json({chat});
+  // } catch (error) {
+  //   console.error("Error creating chat:", error);
+  //   return NextResponse.json(error);
+  // }
 }
