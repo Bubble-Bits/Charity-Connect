590bb03db077ddbff3b49458f93f33b8c85ec38a
Anthony Bui <anthonyb50@gmail.com>
2023-06-29 09:21:15 -0400

    Complete claim button component frontend

--- charity-connect/app/components/Donation/ClaimButton.tsx
+++ charity-connect/app/components/Donation/ClaimButton.tsx
@@ -1,11 +1,20 @@
 "use client";
 import React from "react";
+import axios from "axios";
 
-type Props = {};
+type Props = {
+  itemId: number
+  style: string
+};
 
-function ClaimButton({}: Props) {
+function ClaimButton({ itemId, style }: Props) {
+
+  const claimItem = async (item: number) => {
+    await axios.put('/api/items', { item })
+  }
+
   return (
-    <button>Claim</button>
+    <button className={`${style}`} onClick={()=>{claimItem(itemId)}}>Claim</button>
   );
 }
 
