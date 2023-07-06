99edf7479435cf052d5f99fdd60c3448522f8985
Alvino Handiman <alvinohandiman004@gmail.com>
2023-07-06 19:57:00 -0400

    Added temporary second banner for page to donat. Also revised styles of homepage, the pagetsx file, so that it looks better on a phone

--- charity-connect/app/page.tsx
+++ charity-connect/app/page.tsx
@@ -14,20 +14,26 @@ export default function Home() {
       <div className="DonatinBanner w-screen h-[50vh] bg-[#01002e] text-white">
         <div className="w-screen h-2/3 flex justify-between">
           <div className="w-1/3 flex flex-col items-center">
-            <h3 className="text-white mb-5 md:text-8xl text-5xl">Discover donations near you.</h3>
-            <div className="pt-20">
+            <h3 className="text-white mb-5 md:text-8xl text-4xl ml-4">Discover donations near you.</h3>
+            <div className="pt-20 ml-[15vh]">
               <Link
                 href="/homepage"
-                className="text-3xl mr-5 bg-green-500 border border-solid border-white rounded-full p-3 inline-flex items-center justify-center min-w-[20rem]"
+                className="md:text-3xl md:mr-5 md:p-3
+                bg-green-500 border border-solid border-white rounded-full inline-flex items-center justify-center min-w-[20rem]"
               >
                 View Donations
               </Link>
             </div>
           </div>
           <div className="w-2/3 relative">
-            <div className="absolute w-full h-full mt-[4vh] border border-white bg-green-500 top-0 left-0 z-10"></div>
+            <div className="absolute
+            md:w-full md:h-full md:ml-0 md:mt-[4vh]
+            w-[90%] h-[50%] ml-5 mt-[2vh]
+            border border-white bg-green-500 top-0 left-0 z-10"></div>
             <div className="w-full h-full flex justify-around items-center relative z-20">
-              <div className="w-[40vh] h-[40vh] mt-[10vh] shadow-sm shadow-white overflow-hidden">
+              <div className="md:w-[40vh] md:h-[40vh] md:mt-[10vh] md:ml-0
+              w-[20vh] h-[20vh] mt-[-5vh] ml-[2vh]
+              shadow-sm shadow-white overflow-hidden">
                 <Image
                   src="https://images.pexels.com/photos/16787594/pexels-photo-16787594/free-photo-of-home-appliance-in-a-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   height={500}
@@ -36,7 +42,9 @@ export default function Home() {
                   alt="Photo of person giving another person a box of clothing"
                 />
               </div>
-              <div className="w-[40vh] h-[40vh] top-[20%] mt-[10vh] shadow-sm shadow-white overflow-hidden">
+              <div className="md:w-[40vh] md:h-[40vh] md:mt-[10vh]
+              md:shadow-sm md:shadow-white md:overflow-hidden
+              md:block hidden">
                 <Image
                   src="https://images.pexels.com/photos/4210863/pexels-photo-4210863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   height={500}
@@ -54,20 +62,26 @@ export default function Home() {
       <div className="DonatinBanner w-screen h-[50vh] bg-[#01002e] text-white">
         <div className="w-screen h-2/3 flex justify-between">
           <div className="w-1/3 flex flex-col items-center">
-            <h3 className="text-white mb-5 md:text-8xl text-5xl">Discover donations near you.</h3>
-            <div className="pt-20">
+            <h3 className="text-white mb-5 md:text-8xl text-4xl ml-4">Contribute and donate today!</h3>
+            <div className="pt-20 ml-[15vh] md:mt-[10vh]">
               <Link
-                href="/homepage"
-                className="text-3xl mr-5 bg-green-500 border border-solid border-white rounded-full p-3 inline-flex items-center justify-center min-w-[20rem]"
+                href="/donate"
+                className="md:text-3xl md:mr-5 md:p-3
+                bg-green-500 border border-solid border-white rounded-full inline-flex items-center justify-center min-w-[20rem]"
               >
                 View Donations
               </Link>
             </div>
           </div>
           <div className="w-2/3 relative">
-            <div className="absolute w-full h-full mt-[4vh] border border-white bg-green-500 rounded-full top-0 left-0 z-10"></div>
+            <div className="absolute
+            md:w-full md:h-full md:ml-0 md:mt-[4vh]
+            w-[90%] h-[50%] ml-5 mt-[2vh]
+            border border-white bg-green-500 top-0 left-0 z-10"></div>
             <div className="w-full h-full flex justify-around items-center relative z-20">
-              <div className="w-[40vh] h-[40vh] mt-[10vh] rounded-full shadow-sm shadow-white overflow-hidden">
+              <div className="md:w-[40vh] md:h-[40vh] md:mt-[10vh] md:ml-0
+              w-[20vh] h-[20vh] mt-[-5vh] ml-[2vh]
+              shadow-sm shadow-white overflow-hidden">
                 <Image
                   src="https://images.pexels.com/photos/16787594/pexels-photo-16787594/free-photo-of-home-appliance-in-a-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   height={500}
@@ -76,7 +90,9 @@ export default function Home() {
                   alt="Photo of person giving another person a box of clothing"
                 />
               </div>
-              <div className="w-[40vh] h-[40vh] top-[20%] mt-[10vh] rounded-full shadow-sm shadow-white overflow-hidden">
+              <div className="md:w-[40vh] md:h-[40vh] md:mt-[10vh]
+              md:shadow-sm md:shadow-white md:overflow-hidden
+              md:block hidden">
                 <Image
                   src="https://images.pexels.com/photos/4210863/pexels-photo-4210863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   height={500}
