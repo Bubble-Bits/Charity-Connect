06331a3a61e27924c0eefd2097cecd56569da5c6
Wisesofthemall <foxxydieujuste@gmail.com>
2023-07-08 11:12:35 -0400

    Fix email password

--- charity-connect/app/components/UserProfile.tsx
+++ charity-connect/app/components/UserProfile.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import userController from "../controller/userController";
 
 type Props = {
   user: any;
 };
 
 function UserProfile({ user }: Props) {
+  const [photo, setPhoto] = useState("");
   console.log(user);
+  const checkPhoto = async () => {
+    if (!user.photoUrl) {
+      const photo = await userController(user);
+      setPhoto(photo.profilePic);
+    }
+  };
+  checkPhoto();
   return (
     <Link href={`/userprofile?localId=${user.localId}`}>
       <div className="flex flex-row items-center gap-1">
@@ -18,7 +27,7 @@ function UserProfile({ user }: Props) {
             className="rounded-full w-full h-full object-contain"
             width={30}
             height={30}
-            src={user.photoUrl}
+            src={user.photoUrl ? user.photoUrl : photo}
             alt=""
           />
         </div>
